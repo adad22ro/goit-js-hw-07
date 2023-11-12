626e8cd332b958ea5d2a6c04f5d7cfb004de9dfa
adad22ro <gabirusu2000@gmail.com>
2023-11-12 19:43:18 +0200

    Final

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("galleryitem");
+  listItemEl.innerHTML = `<a class='gallerylink' href='${item.original}'>
+        <img class='gallery__image'
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'/>
+    </a>`;
+  listEl.append(listItemEl);
+});
+
+listEl.addEventListener("click", openImageInLightbox);
+
+function openImageInLightbox(event) {
+  const clickedOn = event.target;
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  event.preventDefault();
+  basicLightbox
+    .create(
+      `<img width='1400' height='900' src='${clickedOn.dataset.source}'/>`
+    )
+    .show();
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+console.log(galleryItems);
 
+const listEl = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const listItemEl = document.createElement("li");
+  listItemEl.classList.add("galleryitem");
+  listItemEl.innerHTML = `<a class='gallerylink' href='${item.original}'>
+        <img class='gallery__image'
+        src='${item.preview}'
+        data-source='${item.original}'
+        alt='${item.description}'/>
+    </a>`;
+
+  listEl.append(listItemEl);
+});
+
+listEl.addEventListener("click", openImageInLightbox);
+
+function openImageInLightbox(event) {
+  const clickedOn = event.target;
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  event.preventDefault();
+
+  const lightbox = new SimpleLightbox(".gallery a", {});
+}
 console.log(galleryItems);
